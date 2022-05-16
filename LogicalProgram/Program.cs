e3c92ffc1cf9ce4131cdab093d2f102c0ac3d53e
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-16 22:03:35 +0530

    UC2[Biplab] Add: Perfect number

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -7,8 +7,25 @@ namespace LogicalProgram
         static void Main(string[] args)
         {
 
-            Fibonacci FiboNacci = new Fibonacci();
-            FiboNacci.FibonacciSeries();
+            Console.WriteLine("Please enter number as per options");
+            Console.WriteLine("1: For Flip a coin and find % of Head and Tail");
+            Console.WriteLine("2: For checking leap year");
+            int check = Convert.ToInt32((string)Console.ReadLine());
+            switch (check)
+            {
+
+                case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.FibonacciSeries();
+                    break;
+                case 2:
+                    PerfectNum perfectNum = new PerfectNum();
+                    perfectNum.Perfect_Num();
+                    break;
+                default:
+                    Console.WriteLine("Please SELECT correct number ! ");
+                    break;
+
+            }
         }
-    }
 }
